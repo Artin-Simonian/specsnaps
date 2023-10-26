bb1c3339f7180105975f931f5f8373e0170440d2
Artin-Simonian <artinsimonian@yahoo.com>
2023-10-26 08:53:37 -0700

    review controller

--- controllers/api/reviews.js
+++ controllers/api/reviews.js
@@ -1,22 +1,23 @@
 const PC = require("../../models/pc");
 
-
 const create = async (req, res) => {
-  const PCDetail = await PC.findById(req.params.postId);
-  PCDetail.reviews.push(req.body);
+  const pc = await PC.findById(req.params.postId);
+  pc.reviews.push(req.body);
+  const review = pc.reviews.id(req.params.id);
+
+  if (!review.user.equals(req.user._id))
+    return res.redirect(`/posts/${post._id}`);
+  review.content = req.body.content;
   try {
-    await PCDetail.save();
+    await pc.save();
   } catch (error) {
     console.log("Error");
   }
 };
 
-
-async function deleteReview(){
-
-}
+async function deleteReview() {}
 
 module.exports = {
-    create,
-    deleteReview
+  create,
+  deleteReview,
 };
