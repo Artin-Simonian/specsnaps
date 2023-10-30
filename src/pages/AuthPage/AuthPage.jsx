13cc197ec49d4421df9a0306d56270bdb9e74e01
Artin-Simonian <artinsimonian@yahoo.com>
2023-10-30 16:45:12 -0700

    debugging

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useNavigate } from "react-router-dom";
 import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
+  const navigate = useNavigate();
   return (
     <main className="AuthPage">
       <div className="AuthPageDIV">
         <h1>AuthPage</h1>
       {showSignUp ? (
-        <SignUpForm setUser={setUser} />
+        <SignUpForm setUser={setUser} navigate={navigate} />
       ) : (
         <LoginForm setUser={setUser} />
       )}
